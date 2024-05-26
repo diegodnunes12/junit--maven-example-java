87aad1ff078228ab56653e108bd6186526277a1f
Diego Nunes <diegodnunes12@gmail.com>
2024-05-26 17:34:24 -0300

    criando examplos de testes

--- src/test/java/com/junit/example/PessoaTest.java
+++ src/test/java/com/junit/example/PessoaTest.java
@@ -2,22 +2,42 @@ package com.junit.example;
 
 import java.time.LocalDateTime;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PessoaTest {
     @Test
     void deveCalcularIdadeCorretamente() {
         Pessoa pessoa1 = new Pessoa("Pessoa 1", LocalDateTime.of(2000, 5, 15, 15, 0, 0));
-        Assertions.assertEquals(24, pessoa1.getIdade());
+        assertEquals(24, pessoa1.getIdade());
     }
 
     @Test
     void deveRetornarSeEhMaiorIdade() {
         Pessoa pessoa1 = new Pessoa("Pessoa 1", LocalDateTime.of(2000, 5, 15, 15, 0, 0));
-        Assertions.assertTrue(pessoa1.ehMaiorIdade());
+        assertTrue(pessoa1.ehMaiorIdade());
 
         Pessoa pessoa2 = new Pessoa("Pessoa 2", LocalDateTime.of(2024, 5, 15, 15, 0, 0));
-        Assertions.assertFalse(pessoa2.ehMaiorIdade());
+        assertFalse(pessoa2.ehMaiorIdade());
+    }
+
+    @Test
+    void validarLancamentos() {
+        int[] primeiro = {10, 20, 30, 40, 50};
+        int[] segundo = {10, 20, 30, 40, 50};
+        int[] terceiro = {-1, 5, 3, 8, 50};
+
+        assertArrayEquals(primeiro, segundo);
+        assertNotEquals(primeiro, terceiro);
+    }
+
+    @Test
+    void validarSeObjetoEstaNulo() {
+        Pessoa pessoa = null;
+        assertNull(pessoa);
+
+        pessoa = new Pessoa("Pessoa 1", LocalDateTime.of(2000, 5, 15, 15, 0, 0));
+        assertNotNull(pessoa);
     }
 }
